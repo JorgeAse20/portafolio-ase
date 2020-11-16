f7b5f00704b596bb54d11e994166c89abd21012f
Jorge Suarez <jorge-2097@hotmail.com>
2020-11-16 16:21:05 -0600

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,17 +10,29 @@ export class InfoPaginaService {
 
   info: InfoPagina = {} ;
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
 
-    console.log('servicio listo');
-
-    this.http.get('assets/data/data-pagina.json')
-    .subscribe ((resp: InfoPagina) => {
-      this.cargada = true;
-      this.info = resp;
-     console.log(resp);
-    });
+ this.cargarInfo();
+ this.cargarEquipo();
     
+   }
+
+   private cargarInfo(){ 
+       this.http.get('assets/data/data-pagina.json')
+   .subscribe ((resp: InfoPagina) => {
+     this.cargada = true;
+     this.info = resp;
+ 
+   });
+   }
+   private cargarEquipo() {
+    this.http.get('https://angular-htmal-ase.firebaseio.com/equipo.json')
+    .subscribe ((resp: any[]) => {
+     this.equipo = resp;
+    // console.log(resp);
+    });
+
    }
 }
